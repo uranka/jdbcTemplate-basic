031aaca9f2b617f58794f6dc594e996befe26856
uranka <jelenagavanski@gmail.com>
2017-10-20 18:42:33 +0200

    Implements find (by name) using named parameters

--- springbookch4/src/main/java/com/wiley/beginningspring/ch4/AccountDaoJdbcImpl.java
+++ springbookch4/src/main/java/com/wiley/beginningspring/ch4/AccountDaoJdbcImpl.java
@@ -2,17 +2,21 @@ package com.wiley.beginningspring.ch4;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 public class AccountDaoJdbcImpl implements AccountDao {
 
     private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
     }
 
     public void insert(Account account) {
@@ -48,13 +52,26 @@ public class AccountDaoJdbcImpl implements AccountDao {
                 accountId);
     }
 
-
     public List<Account> find(List<Long> accountIds) {
         return null;
     }
 
     public List<Account> find(String ownerName) {
-        return null;
+
+        return namedParameterJdbcTemplate.query(
+                "select id,owner_name,balance,access_time,locked from account where owner_name = :ownerName"
+                , Collections.singletonMap("ownerName", ownerName),
+                new RowMapper<Account>() {
+                    public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Account account = new Account();
+                        account.setId(rs.getLong("id"));
+                        account.setOwnerName(rs.getString("owner_name"));
+                        account.setBalance(rs.getDouble("balance"));
+                        account.setAccessTime(rs.getTimestamp("access_time"));
+                        account.setLocked(rs.getBoolean("locked"));
+                        return account;
+                    }
+                });
     }
 
     public List<Account> find(boolean locked) {
--- springbookch4/src/main/java/com/wiley/beginningspring/ch4/Main.java
+++ springbookch4/src/main/java/com/wiley/beginningspring/ch4/Main.java
@@ -1,6 +1,7 @@
 package com.wiley.beginningspring.ch4;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,6 +17,24 @@ public class Main {
         System.out.println(account.getBalance());
         System.out.println(account.getAccessTime());
         System.out.println(account.isLocked());
+
+/*
+        account = accountDao.find("john doe").get(0); // first from list
+        System.out.println(account.getId());
+        System.out.println(account.getOwnerName());
+        System.out.println(account.getBalance());
+        System.out.println(account.getAccessTime());
+        System.out.println(account.isLocked());
+*/
+
+        List<Account> lst = accountDao.find("john doe");
+        for (Account account1 : lst) {
+            System.out.println(account1.getId());
+            System.out.println(account1.getOwnerName());
+            System.out.println(account1.getBalance());
+            System.out.println(account1.getAccessTime());
+            System.out.println(account1.isLocked());
+        }
 
     }
 }
